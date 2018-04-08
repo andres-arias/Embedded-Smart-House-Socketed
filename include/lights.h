7f8cbaac3c7a002f02d9c2b3a792e4664ebca32c
Andres Arias <andres.arias12@gmail.com>
2018-04-07 23:29:27 -0600

    Agregadas luces con biblioteca

--- include/lights.h
+++ include/lights.h
@@ -1,6 +1,8 @@
 #ifndef _LIGHTS_H
 #define _LIGHTS_H
 
+void export_lights(void);
+void unexport_lights(void);
 int light_on(int);
 int light_off(int);
 int light_status(int);
--- src/lights.c
+++ src/lights.c
@@ -4,24 +4,47 @@
 
 int lights[] = {0, 0, 0, 0, 0};
 
+void setup_lights()
+{
+    exportGPIO("5");
+    exportGPIO("6");
+    exportGPIO("13");
+    exportGPIO("19");
+    exportGPIO("26");
+    pinMode("5", "OUT");
+    pinMode("6", "OUT");
+    pinMode("13", "OUT");
+    pinMode("19", "OUT");
+    pinMode("26", "OUT");
+}
+
+void close_lights()
+{
+    unexportGPIO("5");
+    unexportGPIO("5");
+    unexportGPIO("13");
+    unexportGPIO("17");
+    unexportGPIO("26");
+}
+
 int light_on(int light_num)
 {
     switch (light_num)
     {
     case 1:
-        lights[0] = 1;
+        digitalWrite("5", "1");
         return 0;
     case 2:
-        lights[1] = 1;
+        digitalWrite("6", "1");
         return 0;
     case 3:
-        lights[2] = 1;
+        digitalWrite("13", "1");
         return 0;
     case 4:
-        lights[3] = 1;
+        digitalWrite("19", "1");
         return 0;
     case 5:
-        lights[4] = 1;
+        digitalWrite("26", "1");
         return 0;
     }
 }
@@ -31,19 +54,19 @@ int light_off(int light_num)
     switch (light_num)
     {
     case 1:
-        lights[0] = 0;
+        digitalWrite("5", "0");
         return 0;
     case 2:
-        lights[1] = 0;
+        digitalWrite("6", "0");
         return 0;
     case 3:
-        lights[2] = 0;
+        digitalWrite("13", "0");
         return 0;
     case 4:
-        lights[3] = 0;
+        digitalWrite("19", "0");
         return 0;
     case 5:
-        lights[4] = 0;
+        digitalWrite("26", "0");
         return 0;
     }
 }
@@ -53,14 +76,14 @@ int light_status(int light_num)
     switch (light_num)
     {
     case 1:
-        return lights[0];
+        return digitalRead("5");
     case 2:
-        return lights[1];
+        return digitalRead("6");
     case 3:
-        return lights[2];
+        return digitalRead("13");
     case 4:
-        return lights[3];
+        return digitalRead("19");
     case 5:
-        return lights[4];
+        return digitalRead("26");
     }
 }
